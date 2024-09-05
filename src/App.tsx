70338fd435d17cd2b846886795805018ea7312a2
Patrick Tobey <patmictob@gmail.com>
2024-09-05 15:45:23 -0400

    Added MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Button from '@mui/material/Button';
 
 const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with React!</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <Button>MUI Test Button</Button>
     </div>
   );
 };
