d32ba1cac855785fc388ace0cbed4f02b41c8fbd
wreckless-omni <133142730+wreckless-omni@users.noreply.github.com>
2024-09-12 11:28:04 -0400

    initial api review

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LaunchPage from './LaunchPage';
 import MapPage from './MapPage';
 import RecipePage from './RecipePage';
+import AxiosApp from './axiosApp';
 
 const App: React.FC = () => {
   return (
+    
     <Router>
-      <Routes>
-        <Route path="/" element={<LaunchPage/>} />
-        <Route path="/map" element={<MapPage />} />
-        <Route path="/recipes" element={<RecipePage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LaunchPage/>} />
+      <Route path="/map" element={<MapPage />} />
+      <Route path="/recipes" element={<RecipePage />} />
+    </Routes>
+  </Router>
+
+    
   );
 };
 
--- src/RecipePage.tsx
+++ src/RecipePage.tsx
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container, Typography } from '@mui/material';
+import axios from 'axios';
 
 const RecipePage: React.FC = () => {
+
+ const url = 'https://www.usdalocalfoodportal.com/api/farmersmarket/?apikey=U0lsUI6Xi9&x=-84&y=42&state=fl'
+ const [data, setData] = useState([])
+
+ 
+  const fetchInfo = () => {
+    return axios.get(url).then((res) => setData(res.data));
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+
+
   return (
+
+    
     <Container>
       <Typography variant="h4">Searchable Recipe Finder</Typography>
       <Typography>
         This is where the recipe search functionality will be displayed.
+
+        <center>
+        {data.map((dataObj, index) => {
+          return (
+            <div
+              style={{
+                width: "15em",
+                backgroundColor: "#CD8FFD",
+                padding: 2,
+                borderRadius: 10,
+                marginBlock: 10,
+              }}
+            >
+              <p style={{ fontSize: 20, color: 'white' }}>{dataObj.listing_name}</p>
+            </div>
+          );
+        })}
+      </center>
       </Typography>
+
+   
     </Container>
+
+
+
   );
 };
 
