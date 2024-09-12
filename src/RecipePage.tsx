769c28887b8d661c56d08f9af77a685eb750e87a
Patrick Tobey <36872401+ptobey@users.noreply.github.com>
2024-09-12 17:47:12 -0400

    Got API data printing

--- src/RecipePage.tsx
+++ src/RecipePage.tsx
@@ -2,40 +2,43 @@ import React, {useState, useEffect} from 'react';
 import { Container, Typography } from '@mui/material';
 import axios from 'axios';
 
+type FarmersMarket = {
+  brief_des: String;
+  location_street: String
+}
+
 const RecipePage: React.FC = () => {
 
  const url = 'https://www.usdalocalfoodportal.com/api/farmersmarket/?apikey=U0lsUI6Xi9&state=fl'
- const [data, setData] = useState([])
- const [result, setResult] = useState(null)
-
+ const [result, setResult] = useState(undefined as unknown as FarmersMarket[])
  
   const fetchInfo = () => {
-    return axios.get(url).then((result) => setResult(result));
+    return axios.get(url).then((response: any) => {
+      setResult(response.data.data)
+      console.log(response)
+    });
   };
 
   useEffect(() => {
     fetchInfo();
   }, []);
 
-
-
-  return (
-
-    
+  let el = <>
     <Container>
       <Typography variant="h4">Searchable Recipe Finder</Typography>
       <Typography>
         This is where the recipe search functionality will be displayed.
-        <p>{result}</p>
         
       </Typography>
 
    
     </Container>
+    {result && result.length > 0 ? result.map((item, index) => (
+      <p key={index}>{item.location_street}</p>
+    )) : <p>Loading...</p>}
+      </>;
 
-
-
-  );
+      return el;
 };
 
 export default RecipePage;
