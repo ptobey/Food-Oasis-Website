c0eead4e513fcbfffce4044596a9e2ff3a6e21a8
Patrick Tobey <36872401+ptobey@users.noreply.github.com>
2024-09-12 20:33:55 -0400

    Got map working with farmer market API

--- src/MapPage.tsx
+++ src/MapPage.tsx
@@ -1,20 +1,42 @@
 //import "./styles.css";
+import axios from "axios";
 import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-
 function MapPage() {
+  const url = 'https://www.usdalocalfoodportal.com/api/farmersmarket/?apikey=U0lsUI6Xi9&state=fl'
+  const [results, setResults] = useState(undefined as unknown as any[])
+  
+   const fetchInfo = () => {
+     return axios.get(url).then((response: any) => {
+       setResults(response.data.data)
+       console.log(response)
+     });
+   };
+ 
+   useEffect(() => {
+     fetchInfo();
+   }, []);
+
+   useEffect(() => {
+    console.log(results ? results[0].location_y : "hi")
+    console.log(results ? results[0].location_x: "hello")
+  }, [results]);
+
   return (
     <MapContainer center={[28.5384, -81.3789]} zoom={13} style={{ height: '500px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[28.5384, -81.3789]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+        {results ? results.map((result, index) => (
+        <Marker key={index} position={[result.location_y as number, result.location_x as number]}><Popup>
+        <a href={result.media_website}>{result.media_website}</a>
+      </Popup></Marker>
+      
+      )) : <p>Loading...</p>}
+
     </MapContainer>
   );
 
