e44be1b59d2218dc53823bd5340c09a932468474
Patrick Tobey <36872401+ptobey@users.noreply.github.com>
2024-09-12 20:10:19 -0400

    Got map working

--- src/MapPage.tsx
+++ src/MapPage.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+//import "./styles.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-const MapPage: React.FC = () => {
+
+function MapPage() {
   return (
-    <Container>
-      <Typography variant="h4">Map of Nearby Food Suppliers</Typography>
-      <Typography>
-        This is where the map will be displayed.
-      </Typography>
-    </Container>
+    <MapContainer center={[28.5384, -81.3789]} zoom={13} style={{ height: '500px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[28.5384, -81.3789]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
-};
+
+} 
 
 export default MapPage;
