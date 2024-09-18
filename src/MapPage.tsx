62bde4844f8fc9b45382d22124bd2ae32f3a3a7b
gquirk3 <112341590+gquirk3@users.noreply.github.com>
2024-09-18 16:33:47 -0400

    added geolocation
    no errors - but not working as intended or maybe at all

--- src/MapPage.tsx
+++ src/MapPage.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import "leaflet/dist/leaflet.css";
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { Icon } from "leaflet";
+import { useEffect, useState, useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent, useMapEvents } from "react-leaflet";
+import { Icon, LatLng} from "leaflet";
+import Map from 'react-leaflet';
+import L from 'leaflet';
 import MarkerClusterGroup from "react-leaflet-cluster";
 
 function MapPage() {
@@ -14,16 +16,55 @@ function MapPage() {
     iconSize: [38, 38] // size of icon
   });
   
-   const fetchInfo = () => {
+  const fetchInfo = () => {
      return axios.get(url).then((response: any) => {
        setResults(response.data.data)
        console.log(response)
      });
    };
+
+   
+  const mapRef = useRef<L.Map | null>(null);
+   
  
    useEffect(() => {
-     fetchInfo();
+     fetchInfo();  // grabbing market data
+     if (mapRef.current) {  //geo location attempt starts here - maybe need a separate useEffect? maybe something added in the return?
+      const map = mapRef.current;
+
+      map.locate({
+        setView: true,
+      })
+
+      map.on('locationfound', handleOnLocationFound);
+      map.on('locationerror', handleOnLocationError);
+
+      return () => {
+        map.off('locationfound', handleOnLocationFound);
+        map.off('locationerror', handleOnLocationError);
+
+      };
+
+     }
+
+
    }, []);
+  
+   function handleOnLocationFound(event: L.LocationEvent) { // functions for useEffect
+    if (mapRef.current) {
+      const map = mapRef.current;
+      const latlng = event.latlng;
+      const radius = event.accuracy;
+
+      // Create a circle for the located area
+      const circle = L.circle(latlng, { radius });
+      circle.addTo(map);
+      }
+    }
+        
+    function handleOnLocationError(error: L.ErrorEvent) {
+      alert(`Unable to determine location: ${error.message}`);
+    }
 
   return (
     <MapContainer center={[28.5384, -81.3789]} zoom={13} style={{ height: '100vh', width: '100vw', margin: -8  }}>
@@ -31,7 +72,9 @@ function MapPage() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <MarkerClusterGroup>
+    <MarkerClusterGroup
+        chunkedLoading  //supposed to speed up loading
+      >
         {results ? results.map((result, index) => (
         <Marker key={index} position={[result.location_y as number, result.location_x as number]} icon={customIcon}>
           <Popup><a href={result.media_website}>{result.media_website}</a></Popup>
